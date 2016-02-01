a89a686dd9351d433ee54b413b6c3dfd91a012a4
Mattias Cederlund <mattec92@hotmail.com>
2016-02-01 21:40:53 +0100

    Added appbar and left menu drawer. Removed querykey from url.

--- app/components/app.js
+++ app/components/app.js
@@ -1,19 +1,63 @@
 import React from 'react';
-import Header from './header.js';
-import Lead from './lead.js';
-import CardContainer from './cardcontainer.js';
-import ContactContainer from './contactcontainer.js';
-import Footer from './footer.js';
+import Body from './body.js';
+import AppBar from 'material-ui/lib/app-bar';
+import LeftNav from 'material-ui/lib/left-nav';
+import MenuItem from 'material-ui/lib/menus/menu-item';
 
-let App = React.createClass({
+let App = React.createClass({	
+	getInitialState() {
+    	return {
+    		open: false,
+    	};
+  	},
+
+	toggleLeftNav() {
+		this.setState({open: !this.state.open});
+	},
+
 	render() {
+		const styles = {
+			root: {
+				position: 'relative',
+			},
+			appbar: {
+				position: 'absolute',
+				top: 0,
+				backgroundColor: 'rgba(0,0,0,0)',
+			},
+			body: {
+				position: 'absolute',
+				top: 0,
+			},
+		};
+
 	    return (
-		    <div>
-		    	<Header />
-				<Lead />
-				<CardContainer /> 
-				<ContactContainer />
-		    	<Footer />
+		    <div
+		    	style={styles.root}>
+    			<Body 
+    				style={styles.body}/>
+		    	<AppBar
+		    		style={styles.appbar}
+					zDepth={0}
+    				iconClassNameRight="muidocs-icon-navigation-expand-more"
+    				onLeftIconButtonTouchTap={this.toggleLeftNav}/>
+        		<LeftNav 
+        			docked={false}
+					open={this.state.open}
+					onRequestChange={open => this.setState({open})}>
+					<MenuItem>
+						Start
+					</MenuItem>
+					<MenuItem>
+						Menu Item 1
+					</MenuItem>
+					<MenuItem>
+						Menu Item 2
+					</MenuItem>
+					<MenuItem>
+						Menu Item 3
+					</MenuItem>
+				</LeftNav>
 		    </div>
 	    )
   	}
--- app/main.js
+++ app/main.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, Link} from 'react-router'
-/*import createBrowserHistory from 'history/lib/createBrowserHistory'*/
+import createHashHistory from 'history/lib/createHashHistory';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './components/app.js';
 
 injectTapEventPlugin();
 
 ReactDOM.render((
-  <Router /*history={createBrowserHistory()}*/>
+  <Router history={createHashHistory({queryKey: false})}>
     <Route path="/" component={App} />
   </Router>
 ), document.getElementById('app'));
