c0b45b655fea7e4ca8462bb2b0276d38cd9e1d81
Mattias Cederlund <mattec92@hotmail.com>
2016-02-07 21:51:04 +0100

    Added send email endpoint to server. Added simple validation and snackbar for displaying feedback in contactform.

--- app/components/home/contactform.js
+++ app/components/home/contactform.js
@@ -2,13 +2,21 @@ import React from 'react';
 import TextField from '../../../node_modules/material-ui/lib/text-field';
 import RaisedButton from '../../../node_modules/material-ui/lib/raised-button';
 import Typography from 'material-ui/lib/styles';
+import Snackbar from 'material-ui/lib/snackbar';
+import Popover from 'material-ui/lib/popover/popover';
 
 let ContactForm = React.createClass({
     getInitialState() {
         return {
             email: '',
             subject: '',
-            message: ''
+            message: '',
+            evaluatePt1: Math.floor((Math.random() * 10) + 1),
+            evaluatePt2: Math.floor((Math.random() * 10) + 1),
+            evaluateResult: '',
+            snackbarOpen: false,
+            snackbarMessage: '',
+            popoverOpen: false
         };
     },
 
@@ -24,8 +32,111 @@ let ContactForm = React.createClass({
         this.setState({message: e.target.value})
     },
 
-    handleSend() {
-        alert("This function is not yet completed. You entered: " + this.state.email + " " + this.state.subject + " " + this.state.message);
+    handleEvaluationChange(e) {
+        this.setState({
+            evaluateResult: e.target.value
+        });
+    },
+
+    sendEmail() {
+        const postData = {
+            email: this.state.email,
+            subject: this.state.subject,
+            message: this.state.message,
+            evaluatePt1: this.state.evaluatePt1,
+            evaluatePt2: this.state.evaluatePt2,
+            evaluateResult: this.state.evaluateResult
+        };
+
+        $.ajax({
+            url: '/api/contact',
+            dataType: 'json',
+            type: 'POST',
+            data: postData,
+            success: function (data) {
+                this.openSnackbar('Success: ' + data.result);
+                this.resetContactForm();
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error('/api/contact', status, err.toString());
+                this.openSnackbar('Error: ' + err.error);
+            }.bind(this)
+        });
+    },
+
+    resetContactForm() {
+        this.setState({
+            email: '',
+            subject: '',
+            message: ''
+        });
+    },
+
+    openPopover(event) {
+        this.setState({
+            popoverOpen: true,
+            anchorEl: event.currentTarget
+        });
+    },
+
+    closePopover() {
+        this.setState({
+            popoverOpen: false
+        });
+    },
+
+    openSnackbar(message) {
+        this.setState({
+            snackbarOpen: true,
+            snackbarMessage: message
+        });
+    },
+
+    closeSnackbar() {
+        this.setState({
+            snackbarOpen: false
+        });
+    },
+
+    generateNewEvaluation() {
+        this.setState({
+            evaluatePt1: Math.floor((Math.random() * 10) + 1),
+            evaluatePt2: Math.floor((Math.random() * 10) + 1),
+            evaluateResult: ''
+        });
+    },
+
+    doLocalEvaluation() {
+        if (!this.state.evaluateResult) {
+            this.openSnackbar("You must answer the question to continue.");
+        }
+        else if ((this.state.evaluatePt1 + this.state.evaluatePt2) == parseInt(this.state.evaluateResult)) {
+            this.closePopover();
+            this.sendEmail();
+            this.generateNewEvaluation();
+        }
+        else {
+            this.openSnackbar("Evaluation failed, " + this.state.evaluatePt1 + " + " + this.state.evaluatePt2 + " is not '" + this.state.evaluateResult + "'.");
+            this.generateNewEvaluation();
+        }
+    },
+
+    doMessageEvaluation(e) {
+        if (!this.state.email) {
+            this.openSnackbar("Email must be supplied.");
+        }
+        else if (!this.state.subject) {
+            this.openSnackbar("Subject must be supplied.");
+        }
+        else if (!this.state.message) {
+            this.openSnackbar("Message must be supplied.");
+        }
+        //else if (!emailValid) { //TODO: Email evaluation
+        //    this.openSnackbar("Email must be valid.");
+        //}
+        else {
+            this.openPopover(e);
+        }
     },
 
     render() {
@@ -44,6 +155,10 @@ let ContactForm = React.createClass({
             button: {
                 marginTop: '30px',
                 marginBottom: '30px'
+            },
+            popover: {
+                textAlign: 'center',
+                padding: 20
             }
         };
 
@@ -77,7 +192,35 @@ let ContactForm = React.createClass({
                     style={styles.button}
                     label="Send"
                     secondary={true}
-                    onClick={this.handleSend}/>
+                    onClick={this.doMessageEvaluation}/>
+                <Popover
+                    open={this.state.popoverOpen}
+                    anchorEl={this.state.anchorEl}
+                    anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+                    targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                    onRequestClose={this.closePopover}>
+                    <div
+                        style={styles.popover}>
+                        <p>
+                            What is {this.state.evaluatePt1} + {this.state.evaluatePt2}?
+                        </p>
+                        <TextField
+                            style={styles.textField}
+                            onChange={this.handleEvaluationChange}
+                            value={this.state.evaluateResult}
+                            onEnterKeyDown={this.doLocalEvaluation}/>
+                        <br/>
+                        <RaisedButton
+                            secondary={true}
+                            label="Submit"
+                            onClick={this.doLocalEvaluation}/>
+                    </div>
+                </Popover>
+                <Snackbar
+                    open={this.state.snackbarOpen}
+                    message={this.state.snackbarMessage}
+                    autoHideDuration={4000}
+                    onRequestClose={this.closeSnackbar}/>
             </div>
         );
     }
--- server.js
+++ server.js
@@ -1,13 +1,52 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 8080));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', function(req, res) {
-	res.send('Hello World!');
+app.get('/api/hello', function (req, res) {
+    res.send('Hello World!');
 });
 
-app.listen(app.get('port'), function() {
-	console.log('Server started: http://localhost:' + app.get('port') + '/');
+app.post('/api/contact', function (req, res) {
+    const email = req.body.email;
+    const subject = req.body.subject;
+    const message = req.body.message;
+    const evaluatePt1 = req.body.evaluatePt1;
+    const evaluatePt2 = req.body.evaluatePt2;
+    const evaluateResult = req.body.evaluateResult;
+
+    if (evaluatePt1 + evaluatePt2 !== evaluateResult) {
+        res.status(400).json({error: 'Evaluation failed'});
+        return;
+    }
+
+    if (!email) {
+        res.status(400).json({error: 'Email not provided'});
+        return;
+    }
+
+    if (!subject) {
+        res.status(400).json({error: 'Subject not provided'});
+        return;
+    }
+
+    if (!message) {
+        res.status(400).json({error: 'Message not provided'});
+        return;
+    }
+
+    //TODO: Do email validation
+    //if (!emailValid) {
+    //
+    //}
+
+    res.json({result: 'Email was sent'});
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
